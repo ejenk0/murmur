a516451f7773db8975f6555a3bbb40b72261f841
ejenk0 <evynjenkins01@gmail.com>
2020-12-02 16:36:35 +1000

    Volumes work

--- src/App.js
+++ src/App.js
@@ -1,37 +1,64 @@
 import "./App.css";
+import React from "react";
 import volumeicon from "./volume-toggle.png";
+import waves from "./audio/waves.wav";
+import fire from "./audio/fire.wav";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBurn, faWater } from "@fortawesome/free-solid-svg-icons";
 
-const sound_slot = (icon, text) => (
-  <div className="App-sound-slot">
-    <FontAwesomeIcon icon={icon} className="App-sound-icon" />
-    <h3>{text}</h3>
-    <input
-      type="range"
-      min="0"
-      max="100"
-      defaultValue="0"
-      className="App-volume-slider"
-    />
-  </div>
-);
+class SoundSlot extends React.Component {
+  render() {
+    return (
+      <div className="App-sound-slot">
+        <FontAwesomeIcon icon={this.props.icon} className="App-sound-icon" />
+        <h3>{this.props.text}</h3>
+        <input
+          type="range"
+          min="0"
+          max="100"
+          defaultValue="0"
+          className="App-volume-slider"
+        />
+      </div>
+    );
+  }
+}
 
-// NOTE, need to use getElementsByClassName() instead of Id!!!
-
-// var global_volume = 50;
+var global_volume = 50;
+const audio_players = { Fire: new Audio(fire), Waves: new Audio(waves) };
+for (var key in audio_players) {
+  var value = audio_players[key];
+  value.autoplay = 1;
+  value.loop = 1;
+  value.volume = 0;
+  value.play();
+}
 
 window.onload = function () {
-  document.getElementById("volume").oninput = function handleGlobalVolume() {
-    // global_volume = this.value;
-  };
   const sound_sliders = document.getElementsByClassName("App-volume-slider");
+  function handleSoundVolume() {
+    this.previousSibling.previousSibling.style.opacity =
+      (this.value / 100) * 0.8 + 0.2;
+    audio_players[this.previousSibling.textContent].volume =
+      (global_volume / 100) * (this.value / 100);
+  }
+  document.getElementById("volume").oninput = function handleGlobalVolume() {
+    global_volume = this.value;
+    for (i = 1; i < sound_sliders.length; i++) {
+      console.log(
+        "Setting sum volume of " +
+          sound_sliders[i].previousSibling.textContent +
+          " to " +
+          String((global_volume / 100) * (sound_sliders[i].value / 100))
+      );
+      audio_players[sound_sliders[i].previousSibling.textContent].volume =
+        (global_volume / 100) * (sound_sliders[i].value / 100);
+      console.log(audio_players[sound_sliders[i].previousSibling.textContent]);
+    }
+  };
   var i;
-  for (i = 0; i < sound_sliders.length; i++) {
-    sound_sliders[i].oninput = function handleSoundVolume() {
-      this.previousSibling.previousSibling.style.opacity =
-        (this.value / 100) * 0.8 + 0.2;
-    };
+  for (i = 1; i < sound_sliders.length; i++) {
+    sound_sliders[i].oninput = handleSoundVolume;
   }
 };
 
@@ -59,8 +86,8 @@ function App() {
         <h1>MURMUR</h1>
         <h2>Wash away distractions with ambient sound</h2>
         <div className="App-sounds">
-          {sound_slot(faBurn, "Fire")}
-          {sound_slot(faWater, "Waves")}
+          <SoundSlot icon={faBurn} text="Fire" />
+          <SoundSlot icon={faWater} text="Waves" />
         </div>
       </div>
     </div>
